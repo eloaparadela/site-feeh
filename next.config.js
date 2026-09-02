4ca74836ee9884532bb2411600ed2a152d355d55
Eloá Paradela <eloparadela@gmail.com>
2026-09-01 23:45:54 -0300

    Corrige caminhos para GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: isProd ? '/site-feeh' : '',
+  basePath: isProd ? '/prosat' : '',
   images: {
     unoptimized: true,
   },
